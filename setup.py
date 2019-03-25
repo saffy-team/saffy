4a330964cd59fa845d3d6a41de06b4bd41577aac
Paweł <ppierzc@gmail.com>
2019-03-25 09:15:33 +0100

    [FEATURE] change project name to small letter in docs

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 setup(
 	name='saffy',
 	packages=find_packages(),
-	version='0.1.9',
+	version='0.1.1',
 	license='MIT',
 	description='Signal Analysis Framework For You',
 	author='Paweł A. Pierzchlewicz',
